4e7e0ab4c58fa624703c74ae92816947814dcb58
nithiink <nithiin.kathiresan@gmail.com>
2021-09-11 14:10:48 +0530

    NewPackage Screen Basic Setup

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFE9891C);
+const secondaryColor = Color(0xFF46DCD7);
---
+++ lib/screens/newPackage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NewPackage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
